8c097d9a658b2446073d3c773e51a52ce03c92c4
hoyin29 <hoyin29@gmail.com>
2014-04-17 00:40:24 -0700

    user model

---
+++ src/edu/gatech/nutrack/model/User.java
@@ -0,0 +1,45 @@
+package edu.gatech.nutrack.model;
+
+public class User {
+	private String username, password, email;
+	private int type;
+	
+	public User(String username, String password, String email, int type) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.type = type;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+}
