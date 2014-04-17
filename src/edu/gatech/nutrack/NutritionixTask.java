145c2b05f7a0a62357b50251adf23b44c1b9caa4
hoyin29 <hoyin29@gmail.com>
2014-04-17 02:05:35 -0700

    updated task to format data

--- src/edu/gatech/nutrack/NutritionixTask.java
+++ src/edu/gatech/nutrack/NutritionixTask.java
@@ -14,6 +14,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import edu.gatech.nutrack.database.NutritionDataSource;
+import edu.gatech.nutrack.model.Nutrition;
+
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -24,9 +28,8 @@ public class NutritionixTask extends AsyncTask<String, String, String> {
 	private static String id;
 	private static String key;	
 	protected AsyncResponse delegate = null;
-	
-	public NutritionixTask(String id, String key)
-	{
+
+	public NutritionixTask(String id, String key) {
 		this.id = id;
 		this.key = key;
 	}
@@ -36,6 +39,10 @@ public class NutritionixTask extends AsyncTask<String, String, String> {
 		 Log.d(TAG, "inside onPreExecute");
 	 }
 	 
+	 /*
+	  @Override
+		protected String doInBackground(String... params) {	
+	  */
 	@Override
 	protected String doInBackground(String... params) {
 		Log.d(TAG, "inside doInBackground");
@@ -81,65 +88,78 @@ public class NutritionixTask extends AsyncTask<String, String, String> {
 		        	StringBuilder n = new StringBuilder();
 		        	JSONObject jobj = new JSONObject(result);
 		        	
+		        	Nutrition nu = new Nutrition();
+		        	
 		        	Log.d(TAG, "Item Name: " + jobj.getString("item_name"));
 		        	n.append("Item Name: ");
-		        	n.append(formatData(jobj.getString("item_name")));
+		        	n.append(formatStringData(jobj.getString("item_name")));
 		        	n.append("\n");
+		        	nu.setFoodName(formatStringData(jobj.getString("item_name")));
 		        	
 		        	Log.d(TAG, "Brand_name: " + jobj.getString("brand_name"));
 		        	n.append("Brand name: ");
-		        	n.append(formatData(jobj.getString("brand_name")));
+		        	n.append(formatStringData(jobj.getString("brand_name")));
 		        	n.append("\n");
-
+		        	
 		        	Log.d(TAG, "Calories: " + jobj.getString("nf_calories"));
 		        	n.append("Calories: ");
-		        	n.append(formatData(jobj.getString("nf_calories")));
+		        	n.append(formatStringData(jobj.getString("nf_calories")));
 		        	n.append("\n");
+		        	nu.setCalories(formatIntegerData(jobj.getString("nf_calories")));
 		        	
 		        	Log.d(TAG, "Total fat: " + jobj.getString("nf_total_fat"));
 		        	n.append("Total fat: ");
-		        	n.append(formatData(jobj.getString("nf_total_fat")));
+		        	n.append(formatStringData(jobj.getString("nf_total_fat")));
 		        	n.append("\n");
+		        	nu.setTotalFat(formatIntegerData(jobj.getString("nf_total_fat")));
 		        	
 		        	Log.d(TAG, "Saturated fat: " + jobj.getString("nf_saturated_fat"));
 		        	n.append("Saturated fat: ");
-		        	n.append(formatData(jobj.getString("nf_saturated_fat")));
+		        	n.append(formatStringData(jobj.getString("nf_saturated_fat")));
 		        	n.append("\n");
+		        	nu.setSatFat(formatIntegerData(jobj.getString("nf_saturated_fat")));
 		        	
 		        	Log.d(TAG, "Trans fat: " + jobj.getString("nf_trans_fatty_acid"));
 		        	n.append("Trans fat: ");
-		        	n.append(formatData(jobj.getString("nf_trans_fatty_acid")));
+		        	n.append(formatStringData(jobj.getString("nf_trans_fatty_acid")));
 		        	n.append("\n");
+		        	nu.setTotalFat(formatIntegerData(jobj.getString("nf_trans_fatty_acid")));
 		        	
 		        	Log.d(TAG, "Protein: " + jobj.getString("nf_protein"));
 		        	n.append("Protein: ");
-		        	n.append(formatData(jobj.getString("nf_protein")));
+		        	n.append(formatStringData(jobj.getString("nf_protein")));
 		        	n.append("\n");
+		        	nu.setProtein(formatIntegerData(jobj.getString("nf_protein")));
 		        	
 		        	Log.d(TAG, "Total Carbohydrate: " + jobj.getString("nf_total_carbohydrate"));
 		        	n.append("Total Carbohydrate: ");
-		        	n.append(formatData(jobj.getString("nf_total_carbohydrate")));
+		        	n.append(formatStringData(jobj.getString("nf_total_carbohydrate")));
 		        	n.append("\n");
+		        	nu.setTotalCarb(formatIntegerData(jobj.getString("nf_total_carbohydrate")));
 		        	
 		        	Log.d(TAG, "Dietary Fiber: " + jobj.getString("nf_dietary_fiber"));
 		        	n.append("Dietary Fiber: ");
-		        	n.append(formatData(jobj.getString("nf_dietary_fiber")));
+		        	n.append(formatStringData(jobj.getString("nf_dietary_fiber")));
 		        	n.append("\n");
+		        	nu.setDietFiber(formatIntegerData(jobj.getString("nf_dietary_fiber")));
 		        	
 		        	Log.d(TAG, "Sugars: " + jobj.getString("nf_sugars"));
 		        	n.append("Sugars: ");
-		        	n.append(formatData(jobj.getString("nf_sugars")));
+		        	n.append(formatStringData(jobj.getString("nf_sugars")));
 		        	n.append("\n");
+		        	nu.setSugars(formatIntegerData(jobj.getString("nf_sugars")));
 		        	
 		        	Log.d(TAG, "Sodium: " + jobj.getString("nf_sodium"));
 		        	n.append("Sodium: ");
-		        	n.append(formatData(jobj.getString("nf_sodium")));
+		        	n.append(formatStringData(jobj.getString("nf_sodium")));
 		        	n.append("\n");
+		        	nu.setSodium(formatIntegerData(jobj.getString("nf_sodium")));
 		        	
 		        	Log.d(TAG, "Cholesterol: " + jobj.getString("nf_cholesterol"));
 		        	n.append("Cholesterol: ");
-		        	n.append(formatData(jobj.getString("nf_cholesterol")));
+		        	n.append(formatStringData(jobj.getString("nf_cholesterol")));
 		        	n.append("\n");
+		        	nu.setCholesterol(formatIntegerData(jobj.getString("nf_cholesterol")));
 		        	
 		        	result = n.toString();
 				} catch (JSONException e1) {
@@ -182,9 +202,15 @@ public class NutritionixTask extends AsyncTask<String, String, String> {
 		return s;
 	}
 	
-	private String formatData(String s) {
+	private String formatStringData(String s) {
 		if(s.equals("null"))
 			return "N/A";
 		return s;
+	}
+	
+	private int formatIntegerData(String s) {
+		if(s.equals("null")) 
+			return -1;
+		return Integer.valueOf(s);
 	}
 }
