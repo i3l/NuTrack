ec05b923a404eb97cc1c3e5e941ba4d76002841f
hoyin29 <hoyin29@gmail.com>
2014-04-17 02:12:10 -0700

    updated delegator interface

--- src/edu/gatech/nutrack/AsyncResponse.java
+++ src/edu/gatech/nutrack/AsyncResponse.java
@@ -1,6 +1,9 @@
 package edu.gatech.nutrack;
 
+import edu.gatech.nutrack.model.Nutrition;
+
 
 public interface AsyncResponse {
-	void getAsyncResponse(String s);
+	//void getAsyncResponse(String s);
+	void getAsyncResponse(Nutrition n);
 }
