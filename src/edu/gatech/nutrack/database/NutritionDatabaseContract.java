55bf2541a2b099e1519fe166e31f74834d5c83a1
hoyin29 <hoyin29@gmail.com>
2014-04-17 01:12:09 -0700

    nut db contract

---
+++ src/edu/gatech/nutrack/database/NutritionDatabaseContract.java
@@ -0,0 +1,24 @@
+package edu.gatech.nutrack.database;
+
+import android.provider.BaseColumns;
+
+public abstract class NutritionDatabaseContract implements BaseColumns
+{
+    public static final String TABLE_NAME = "nutrition";
+    public static final String COLUMN_NAME_FOOD_NAME = "food_name";
+    public static final String COLUMN_NAME_CALORIES = "calories";
+    public static final String COLUMN_NAME_TOTAL_FAT = "total_fat";
+    public static final String COLUMN_NAME_SAT_FAT = "sat_fat";
+    public static final String COLUMN_NAME_TRANS_FAT = "trans_fat";
+    public static final String COLUMN_NAME_PROTEIN = "protein";
+    public static final String COLUMN_NAME_TOTAL_CARB = "total_carb";
+    public static final String COLUMN_NAME_DIET_FIBER = "diet_fiber";
+    public static final String COLUMN_NAME_SUGAR = "sugar";
+    public static final String COLUMN_NAME_SODIUM = "sodium";
+    public static final String COLUMN_NAME_CHOLESTEROL = "cholesterol";
+    
+    private NutritionDatabaseContract()
+    {
+    	
+    }
+}
