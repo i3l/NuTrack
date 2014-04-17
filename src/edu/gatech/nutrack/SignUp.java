76a0347cd60a84117251467c4e6e1a5677f3a2a6
hoyin29 <hoyin29@gmail.com>
2014-04-17 16:59:14 -0700

    need to updated user type

--- src/edu/gatech/nutrack/SignUp.java
+++ src/edu/gatech/nutrack/SignUp.java
@@ -1,13 +1,18 @@
 package edu.gatech.nutrack;
 
+import edu.gatech.nutrack.database.UserDataSource;
+import edu.gatech.nutrack.model.User;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends Activity {
-
+	private static final String TAG = "***SIGNUP";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,9 +26,26 @@ public class SignUp extends Activity {
 		return true;
 	}
 	public void callSignUp(View view) {
-		Intent callSignUpIntent = new Intent(this, Home.class);
-		System.out.println("here");
-		startActivity(callSignUpIntent);
+		TextView un = (TextView) findViewById(R.id.userNameText);
+		TextView pwd = (TextView) findViewById(R.id.passwordText);
+		TextView pwdconf = (TextView) findViewById(R.id.passwordConfText);
+		TextView email = (TextView) findViewById(R.id.emailText);
+		
+		if(pwd.getText().toString().equals(pwdconf.getText().toString())) {
+			User u = new User(un.getText().toString(),
+					pwd.getText().toString(),
+					email.getText().toString(),
+					0);  // change user type by radio button
+			UserDataSource uds = new UserDataSource(this);
+			uds.open();
+			uds.addUser(u);
+			uds.close();
+			
+			Intent callSignUpIntent = new Intent(this, Home.class);
+			startActivity(callSignUpIntent);
+		} else {
+			Log.d(TAG, "cannot sign up because password confirmation failed");
+			finish();
+		}
 	}
-
 }
